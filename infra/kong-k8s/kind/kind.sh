57023e9787c4789ed7480627cb876f79caab72a1
henriquelucasdf <henriquelucasdf@gmail.com>
2022-12-07 21:31:43 -0300

    feat: Add relative path to create kind cluster

--- infra/kong-k8s/kind/kind.sh
+++ infra/kong-k8s/kind/kind.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-kind create cluster --name kong-fc --config cluster_config.yaml
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
+kind create cluster --name kong-fc --config ./cluster_config.yaml
 kubectl cluster-info --context kind-kong-fc

adc8775dfb9b16d552fbff870ad723fb4c087eb6
henriquelucasdf <henriquelucasdf@gmail.com>
2024-04-08 22:20:11 -0300

    feat: add kong and kind sh

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 Kong in Kubernetes
+
+# To reproduce
+```shell
+cd infra/kong-k8s/kind
+
+sh kind.sh
+```
+
+```shell
+cd ../kong
+
+sh kong.sh
+```
--- infra/kong-k8s/kind/kind.sh
+++ infra/kong-k8s/kind/kind.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "$parent_path"
 
 kind create cluster --name kong-fc --config ./cluster_config.yaml
 kubectl cluster-info --context kind-kong-fc
